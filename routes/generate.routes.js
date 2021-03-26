ccb47c9473a3e43348459f2375e68f4820805356
vientrieu <37980891+vientrieu@users.noreply.github.com>
2021-03-26 16:34:05 +0700

    Update generate.routes.js

--- routes/generate.routes.js
+++ routes/generate.routes.js
@@ -4,5 +4,6 @@ const generationController = require('../controller/generation.controller');
 router.get('/category', (req, res) => {generationController.genCategory(req, res)});
 router.get('/traffic-sign', (req, res) => {generationController.genTrifficSign(req, res)});
 router.get('/marker', (req, res) => {generationController.genMarker(req, res)});
+router.get('/marker', (req, res) => {res.send('111111111111111111')});
 // router.post('/', (req, res) => {pointController.save(req, res)});
 module.exports = router;
